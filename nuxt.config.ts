8622541096937bb90ae5ed7edd27c716f484fc9d
mahdiar <kaheh.1384@gmail.com>
2024-02-03 00:07:34 +0330

    feat: some initial setting are added :white_check_mark:

---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  ssr: true,
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+
+    head: {
+      title: process.env.APP_TITLE,
+      htmlAttrs: {
+        lang: "fa",
+        dir: "rtl",
+      },
+      charset: "utf-16",
+      viewport: "width=device-width, initial-scale=1, user-scalable=no",
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=no",
+        },
+        {
+          hid: "description",
+          name: "description",
+          content: process.env.APP_DESCRIPTION,
+        },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+    },
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    container: {
+      center: true,
+
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      primary: {
+        DEFAULT: "#5a4bd2",
+      },
+      secondary: {
+        DEFAULT: "#FBBA00",
+      },
+      dark: {
+        DEFAULT: "#292931",
+      },
+      silver: {
+        DEFAULT: "#EEEEEE",
+        light: "#F8F8F8",
+        dark: "#D4D8DB",
+      },
+      white: "#FFFFFF",
+      black: "#000000",
+    },
+  },
+  plugins: [],
+}
+
