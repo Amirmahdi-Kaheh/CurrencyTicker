bd1868b92e20700a0e87cf74df16b5819a84ae28
mahdiar <kaheh.1384@gmail.com>
2024-02-03 00:06:21 +0330

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col justify-between">
+    <div>
+<!--      <LayoutsTheHeader></LayoutsTheHeader>-->
+      <slot />
+    </div>
+<!--    <LayoutsTheFooter></LayoutsTheFooter>-->
+  </div>
+</template>
+
+<style scoped></style>
