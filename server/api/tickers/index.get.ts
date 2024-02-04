0d562a9822013983fdaf85fcdecd1e0d3a96c46f
mahdiar <kaheh.1384@gmail.com>
2024-02-04 18:21:43 +0330

    feat: change the tickers object key on list

--- server/api/tickers/index.get.ts
+++ server/api/tickers/index.get.ts
@@ -1,9 +1,23 @@
 import call from "~/server/api/plugins/ApiFactory";
+import {getIconUrl} from "~/server/utils/general";
+
+interface IssuedTicker {
+    last: string;
+    open_24h: string;
+    symbol: string;
+}
 
 export default defineEventHandler(async (event) => {
     const response = await call<any>(
         "GET",
         "/oapi/v1/market/tickers",
     );
-    return response.data.tickers;
+
+    return response.data.tickers.map((item: IssuedTicker) => ({
+        last: item.last,
+        open24h: item.open_24h,
+        symbol: item.symbol.split("-")[0],
+        pair: item.symbol.split("-")[1],
+        icon: getIconUrl(item.symbol.split("-")[0].toLowerCase())
+    }));
 });
