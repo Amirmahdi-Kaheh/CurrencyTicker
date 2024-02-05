9d0ea22f3c22c8d368f17b02fb39fd1b52ad2cd6
mahdiar <kaheh.1384@gmail.com>
2024-02-05 18:11:12 +0330

    feat: change header and footer spaces

--- components/layouts/TheFooter.vue
+++ components/layouts/TheFooter.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="pt-20 pb-12 sticky bottom-0 bg-gradient-to-t from-dark from-50% to-transparent pointer-events-none">
+  <div class="pt-12 pb-8 sticky bottom-0 bg-gradient-to-t from-dark from-60% to-transparent pointer-events-none">
     <div>
       قدرت گرفته از اوکی اکسچنج
     </div>
--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="pb-20 pt-12 sticky top-0 bg-gradient-to-b from-dark from-50% to-transparent pointer-events-none z-30">
+  <div class="pb-12 pt-8 sticky top-0 bg-gradient-to-b from-dark from-50% to-transparent pointer-events-none z-30">
     <div class="w-full  flex items-center justify-between pointer-events-auto">
       <nuxt-link to="/">
         <h1 class="font-bold text-lg">رهیاب قیمت</h1>
