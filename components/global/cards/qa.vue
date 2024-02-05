c32a4e9a813269cba076589853f25cf58fece790
mahdiar <kaheh.1384@gmail.com>
2024-02-05 18:16:38 +0330

    feat: change qa component preferences

--- components/global/cards/qa.vue
+++ components/global/cards/qa.vue
@@ -18,9 +18,9 @@ function toggleCollapse() {
 </script>
 
 <template>
-  <div @click="toggleCollapse" class="p-4 rounded-lg bg-border cursor-pointer">
-    <span>{{question}}</span>
-    <div class="text-sm leading-6 mt-2" v-if="!isCollapsed">
+  <div @click="toggleCollapse" class="p-4 rounded-lg bg-border cursor-pointer ">
+    <span class="text-white">{{question}}</span>
+    <div class="text-sm leading-6 mt-2 text-silver-dark text-justify pt-2 border-t border-dark" v-if="!isCollapsed">
       <span class="text-primary ml-1">پاسخ</span>
       {{answer}}
     </div>
