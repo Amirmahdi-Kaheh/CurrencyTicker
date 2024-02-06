322aaafd473347a3c0d0ad3f5ef55cc0159d7568
mahdiar <kaheh.1384@gmail.com>
2024-02-06 07:11:09 +0330

    feat: add logo to header

--- components/layouts/TheHeader.vue
+++ components/layouts/TheHeader.vue
@@ -6,7 +6,8 @@
   <div class="pb-12 pt-8 sticky top-0 bg-gradient-to-b from-dark from-50% to-transparent pointer-events-none z-30">
     <div class="w-full  flex items-center justify-between pointer-events-auto">
       <nuxt-link to="/">
-        <h1 class="font-bold text-lg">رهیاب قیمت</h1>
+        <h1 class="font-bold text-lg hidden">Currency Ticker By Mahdiar Kaheh</h1>
+        <img src="@/assets/img/logo/logo.svg" class="w-10" alt="">
       </nuxt-link>
       <nuxt-link to="/faq" class="btn btn-primary">
         سوالات متداول
